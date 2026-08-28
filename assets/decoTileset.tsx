1abaf3705cb822eca8e353c0cb70f425a31318fc
ManuelCoggiola <manuelcm.xd@gmail.com>
2026-08-27 21:20:40 -0300

    Integración de sistema de combate, construcción, prototipo del mapa y sprites

---
+++ assets/decoTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="decoTileset" tilewidth="32" tileheight="32" tilecount="924" columns="33">
+ <image source="decoTileset.png" width="1076" height="927"/>
+</tileset>
