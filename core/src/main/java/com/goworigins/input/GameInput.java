a0c8d88144eff7749ac4f95d02e40e1311a87a5f
Felipe Pavón Martorelli <felipepavonmartorelli35@gmail.com>
2026-08-30 09:22:26 -0300

    fix: corregir viewport que quedaba mal calculado al maximizar la ventana

--- core/src/main/java/com/goworigins/input/GameInput.java
+++ core/src/main/java/com/goworigins/input/GameInput.java
@@ -40,12 +40,10 @@ public class GameInput extends InputAdapter {
     }
 
     public boolean consumeBuild() {
-
         if (buildPressed) {
             buildPressed = false;
             return true;
         }
-
         return false;
     }
 
@@ -91,20 +89,17 @@ public class GameInput extends InputAdapter {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
         if (button == Input.Buttons.LEFT) {
             attackPressed = true;
         }
-
         return true;
     }
-    public boolean consumeAttack() {
 
+    public boolean consumeAttack() {
         if (attackPressed) {
             attackPressed = false;
             return true;
         }
-
         return false;
     }
 }
